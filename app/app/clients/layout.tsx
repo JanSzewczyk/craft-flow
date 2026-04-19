05336828c04f4ab1d9b1e4d12ebfa075c2741641
JanSzewczyk <jan.szewczyk1997@gmail.com>
2026-04-19 22:02:19 +0200

    feat: replace fragment shorthand with React.Fragment in ClientsLayout component

--- app/app/clients/layout.tsx
+++ app/app/clients/layout.tsx
@@ -2,9 +2,9 @@ import * as React from "react";
 
 export default function ClientsLayout({ children, sheet }: { children: React.ReactNode; sheet: React.ReactNode }) {
   return (
-    <>
+    <React.Fragment>
       {children}
       {sheet}
-    </>
+    </React.Fragment>
   );
 }
